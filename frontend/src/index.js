dfea067ef108ad5921b5da6350381b3fd7bc16d0
Shashi1628 <SHASHIDHARVADLA12@GMAIL.COM>
2022-10-21 16:44:05 +0530

    commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Navigation from './Navigation';
+import ProspectListpage from './ProspectListpage';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navigation />
+    <ProspectListpage />
   </React.StrictMode>
 );
